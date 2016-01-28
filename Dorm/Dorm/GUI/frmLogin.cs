031c39c3ba7745b2c8a2de1bc464e9a853f35d72
epiczeth <epiczeth@hotmail.com>
2016-01-28 17:13:47 +0700

    update

--- Dorm/Dorm/GUI/frmLogin.cs
+++ Dorm/Dorm/GUI/frmLogin.cs
@@ -7,11 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace Dorm
+namespace Dorm.GUI
 {
-    public partial class Form1 : Form
+    public partial class frmLogin : Form
     {
-        public Form1()
+        public frmLogin()
         {
             InitializeComponent();
         }
